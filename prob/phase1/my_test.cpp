c2d27b15207551bd21caecf903a374648149d516
Soumitra <22b0984@iitb.ac.in>
2023-11-11 14:52:54 +0530

    phase1 part1

--- prob/phase1/my_test.cpp
+++ prob/phase1/my_test.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
+#include<string>
 using namespace std;
 int main() {
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    // std::vector<int> numbers = {1, 2, 3, 4, 5};
     
-    // Remove the element at index 2 (3rd element)
-    numbers.erase(numbers.begin() + 2);
+    // // Remove the element at index 2 (3rd element)
+    // numbers.erase(numbers.begin() + 2);
 
-    for (int num : numbers) {
-        std::cout << num << " ";
-    }
-
+    // for (int num : numbers) {
+    //     std::cout << num << " ";
+    // }
+    string a,b;
+    cin>>a>>b;
+    cout<<a<<endl<<b;
     return 0;
 }
