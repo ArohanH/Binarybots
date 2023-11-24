fb9cc32830380e0590a8a6afef5bbd913db0d181
Soumitra <22b0984@iitb.ac.in>
2023-11-24 12:27:59 +0530

    initital phase2 part1

---
+++ test.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Merge two halves of the vector
+void merge(vector<pair<int, int>>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary vectors
+    vector<pair<int, int>> L(n1), R(n2);
+
+    // Copy data to temporary vectors L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temporary vectors back into arr[left..right]
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i].first <= R[j].first) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Merge Sort function
+void mergeSort(vector<pair<int, int>>& arr, int left, int right) {
+    if (left < right) {
+        // Same as (left+right)/2, but avoids overflow for large left and right
+        int mid = left + (right - left) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    // Example usage
+    vector<pair<int, int>> vec = {{0, 5}, {1, 8}, {2, 3}, {4, 1}, {6, 7}};
+    
+    // Calculate the size of the vector
+
+    // Perform merge sort
+    mergeSort(vec, 0, vec.size() - 1);
+
+    // Print the sorted vector
+    for (const auto& p : vec) {
+        cout << "(" << p.first << ", " << p.second << ") ";
+    }
+
+    return 0;
+}
